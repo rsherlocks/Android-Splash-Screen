b3f2516e3ba5cb541c345e58cb721b86571b6ebe
rsherlocks <hasan15-7482@diu.edu.bd>
2020-06-21 23:39:04 +0600

    frist commit

---
+++ app/src/main/java/com/example/androishaper/splashscreen/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.androishaper.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import static java.lang.Thread.sleep;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                    Intent i=new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        });
+        thread.start();
+
+    }
+}
